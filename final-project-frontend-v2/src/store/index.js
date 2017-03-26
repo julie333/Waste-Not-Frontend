c979bc44ec2ebc8139e80bb3167665f49bedd50a
julie <annjulg@gmail.com>
2017-03-26 23:23:52 +0200

    deleted old version

--- final-project-frontend-v2/src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import reducer from './reducers.js';
-import thunk from 'redux-thunk';
-
-let store = createStore(reducer, applyMiddleware(thunk));
-
-export default store;
